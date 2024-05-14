f202fb6f1a13f063d37c12bb67aa62419e3432d4
Cengiz Türkoglu <admin@hanka-trading.com>
2024-05-15 01:30:49 +0200

    Added: DOTENV, Google Strategy

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const passport = require("passport");
+const cors = require("cors");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome</h1>");
-});
+app.use(cors);
+
+require("dotenv").config();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_OAUTH_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
+      callbackUrl: "/auth/google/callback",
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
 
 let PORT = process.env.PORT || 5000;
 
