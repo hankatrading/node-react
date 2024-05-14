d56c6fbe197f94de2be99ecdd6707eccdd3c71db
Cengiz Türkoglu <admin@hanka-trading.com>
2024-05-14 23:49:54 +0200

    Response changed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  "<h1>Welcome</h1>";
+  res.send("<h1>Welcome</h1>");
 });
 
 let PORT = process.env.PORT || 5000;
