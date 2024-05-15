0f482416d6bdbe355af3c6fcfe56f6b2119ad017
hankatrading <70607172+hankatrading@users.noreply.github.com>
2024-05-15 07:38:27 +0000

    Google OAuth Callback

--- index.js
+++ index.js
@@ -22,6 +22,9 @@ passport.use(
   )
 );
 
+app.get("/auth/google", passport.authenticate("google", { scope: ["profile", "email"] }));
+app.get("/auth/google/callback", passport.authenticate("google"));
+
 let PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
