ec26b657806409ff3e3c7ed3e82c4c3ea6a15aa3
hankatrading <70607172+hankatrading@users.noreply.github.com>
2024-06-03 11:46:26 +0000

    d

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
 const express = require("express");
-const passport = require("passport");
 const cors = require("cors");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-
-const app = express();
-
-app.use(cors);
 
 require("dotenv").config();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_OAUTH_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
-      callbackUrl: "/auth/google/callback",
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log("Access Token: ", accessToken);
-      console.log("Refresh Token: ", refreshToken);
-      console.log("Profile: ", profile);
-    }
-  )
-);
+let app = express();
+app.use(cors);
 
-app.get("/auth/google", passport.authenticate("google", { scope: ["profile", "email"] }));
-app.get("/auth/google/callback", passport.authenticate("google"));
+require("./service/passport")(process.env);
+require("./routes/auth")(app);
 
 let PORT = process.env.PORT || 5000;
 
