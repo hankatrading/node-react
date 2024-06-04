3f39b77bd3be9915f9ee72e43b27ea0ec80a4615
hankatrading <70607172+hankatrading@users.noreply.github.com>
2024-06-04 07:20:01 +0000

    MongoDB with mongoose installation completed

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 require("dotenv").config();
+require("./model/User");
+require("./service/passport");
 
 let app = express();
 
 require("./service/passport");
 require("./routes/auth")(app);
+
+let mongoseURL = "mongodb+srv://cengiztuerkoglu:olda@8009VM!@freecluster.35drwct.mongodb.net/?retryWrites=true&w=majority&appName=FreeCluster";
+mongoose.connect(mongoseURL);
 
 let PORT = process.env.PORT || 5000;
 
--- service/passport.js
+++ service/passport.js
@@ -1,5 +1,6 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("../model/User");
 
 passport.use(
   new GoogleStrategy(
@@ -9,9 +10,13 @@ passport.use(
       callbackURL: process.env.GOOGLE_OAUTH_CALLBACK_URL,
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("Access Token: ", accessToken);
-      console.log("Refresh Token: ", refreshToken);
-      console.log("Profile: ", profile);
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id }).save().then(user => done(null, user));
+        }
+      });
     }
   )
 );
