6df40d63e84688c6a4db538965f9ed4abc45b5c7
hankatrading <70607172+hankatrading@users.noreply.github.com>
2024-06-04 08:52:28 +0000

    MongoDB connection implemented, but not completed

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 require("dotenv").config();
-require("./model/User");
-require("./service/passport");
 
 let app = express();
 
+mongoose.connect(process.env.MONGODB_URI);
+
+require("./model/User");
 require("./service/passport");
 require("./routes/auth")(app);
-
-let mongoseURL = "mongodb+srv://cengiztuerkoglu:olda@8009VM!@freecluster.35drwct.mongodb.net/?retryWrites=true&w=majority&appName=FreeCluster";
-mongoose.connect(mongoseURL);
 
 let PORT = process.env.PORT || 5000;
 
