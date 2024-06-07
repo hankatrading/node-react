37d33afa22df3701e427112cfe031debeb6701ae
Cengiz Türkoglu <admin@hanka-trading.com>
2024-06-08 01:23:39 +0200

    Git problem fixed

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  GOOGLE_OAUTH_CLIENT_ID: "849335793283-tn2hcupelmfki25lgngl6r1d75qqns9t.apps.googleusercontent.com",
-  GOOGLE_OAUTH_CLIENT_SECRET: "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",
-  GOOGLE_OAUTH_CALLBACK_URL: "/auth/google/callback",
-  MONGODB_URI:
-    "mongodb+srv://cengiztuerkoglu:Tys6PsrLgmYBN4vJ@emaily-dev.snsu5va.mongodb.net/?retryWrites=true&w=majority&appName=Emaily-Dev",
-  COOKIEKEY: "olda@8009VM!",
-};
