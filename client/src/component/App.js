0e828209371f67048162794a2ae54da288483438
Cengiz Türkoglu <admin@hanka-trading.com>
2024-06-10 21:49:26 +0200

    Components implemented and added to App.js

--- client/src/component/App.js
+++ client/src/component/App.js
@@ -1,10 +1,21 @@
 const React = require("react");
+const { BrowserRouter, Route } = require("react-router-dom");
+
+let Header = () => <h2>Header</h2>;
+let Dashboard = () => <h2>Dashboard</h2>;
+let SurveyNew = () => <h2>SurveyNew</h2>;
+let Landing = () => <h2>Landing</h2>;
 
 let App = () => {
   return (
-    <div id="root">
-      <h1>Hi User</h1>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Route exact={true} path="/" component={Landing} />
+        <Route exact path="/surveys" component={Dashboard} />
+        <Route path="/surveys/new" component={SurveyNew} />
+      </div>
+    </BrowserRouter>
   );
 };
 
