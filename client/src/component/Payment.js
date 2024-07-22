eb68b70b3a010a643869362fa5c1b84cdfe875e9
Cengiz Türkoglu <admin@hanka-trading.com>
2024-07-22 21:17:20 +0200

    Client-side STRIPE Public Keys key renamed

--- client/src/component/Payment.js
+++ client/src/component/Payment.js
@@ -11,7 +11,7 @@ class Payment extends Component {
         description="$5 for 5 email credits"
         amount={500}
         token={(token) => this.props.handleToken(token)}
-        stripeKey={process.env.REACT_APP_STRIPE_PUBLIC_TEST_KEY}
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
       >
         <button className="btn">Add Credits</button>
       </StripeCheckout>
