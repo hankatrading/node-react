4b4ea22c8d44ffe8b5e4238191b89eddf5d2ade1
Cengiz Türkoglu <admin@hanka-trading.com>
2024-08-03 22:14:45 +0200

    API issue for Stripe Charging fixed

--- routes/billing-routes.js
+++ routes/billing-routes.js
@@ -3,27 +3,33 @@ const requireLogin = require("../middleware/requireLogin");
 const stripe = require("stripe")(keys.STRIPE_SECRET_KEY);
 
 module.exports = (app) => {
-  app.post("/api/stripe", requireLogin, async (req, res) => {
-    try {
-      let charge = await stripe.charges.create({
-        amount: 500,
-        currency: "usd",
-        description: "Charging Customer Credits: 5$ for 5 credits",
-        source: req.body.id,
-      });
+  app.post("/api/stripe", async (req, res) => {
+    let isLoggedIn = requireLogin;
+    console.log("Is logged in", isLoggedIn);
+    if (isLoggedIn) {
+      try {
+        let data = {
+          amount: 500,
+          currency: "usd",
+          description: "Charging Customer Credits: 5$ for 5 credits",
+          source: req.body.id,
+        };
 
-      req.user.credits += 5;
+        await stripe.charges.create(data);
 
-      let user = await req.user.save();
-      res.send(user);
-    } catch (err) {
-      let message = "An error occurred while processing your payment.";
+        req.user.credits += 5;
 
-      if (err.type === "StripeCardError") {
-        message = err.message;
+        let user = await req.user.save();
+        res.send(user);
+      } catch (err) {
+        let message = "An error occurred while processing your payment.";
+
+        if (err.type === "StripeCardError") {
+          message = err.message;
+        }
+
+        res.status(500).send(message);
       }
-
-      res.status(500).send(message);
     }
   });
 };
