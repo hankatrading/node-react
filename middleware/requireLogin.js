de25c05c4791d50bbf29b91215d4ff683ff7c169
Cengiz Türkoglu <admin@hanka-trading.com>
2024-08-11 18:54:15 +0200

    Stripe Pending issue fixed

--- middleware/requireLogin.js
+++ middleware/requireLogin.js
@@ -2,4 +2,6 @@ module.exports = (req, res, next) => {
   if (!req.user) {
     return res.status(401).send({ rc: 401, error: "You must logged in" });
   }
+
+  next();
 };
--- routes/billing-routes.js
+++ routes/billing-routes.js
@@ -3,31 +3,29 @@ const requireLogin = require("../middleware/requireLogin");
 const stripe = require("stripe")(keys.STRIPE_SECRET_KEY);
 
 module.exports = (app) => {
-  app.post("/api/stripe", async (req, res) => {
-    if (requireLogin) {
-      try {
-        let data = {
-          amount: 500,
-          currency: "usd",
-          description: "Charging Customer Credits: 5$ for 5 credits",
-          source: req.body.id,
-        };
+  app.post("/api/stripe", requireLogin, async (req, res) => {
+    try {
+      let data = {
+        amount: 500,
+        currency: "usd",
+        description: "Charging Customer Credits: 5$ for 5 credits",
+        source: req.body.id,
+      };
 
-        await stripe.charges.create(data);
+      await stripe.charges.create(data);
 
-        req.user.credits += 5;
+      req.user.credits += 5;
 
-        let user = await req.user.save();
-        res.send(user);
-      } catch (err) {
-        let message = "An error occurred while processing your payment.";
+      let user = await req.user.save();
+      res.send(user);
+    } catch (err) {
+      let message = "An error occurred while processing your payment.";
 
-        if (err.type === "StripeCardError") {
-          message = err.message;
-        }
-
-        res.status(500).send(message);
+      if (err.type === "StripeCardError") {
+        message = err.message;
       }
+
+      res.status(500).send(message);
     }
   });
 };
